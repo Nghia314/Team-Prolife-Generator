f61a0b9fe6ee4a13db71bfa4422e94433eab549c
Nghia Le <davidle314@hotmail.com>
2022-04-24 19:58:28 -0700

    processing engineer.test file

---
+++ __tests__/engineer.test.js
@@ -0,0 +1,18 @@
+const Engineer = require('../library/engineer.js')
+
+describe('should create github for engineer.', () => {
+    const setGitHub = "DavidLe";
+    const e = new Engineer('David', 123, "davidle@hotmail.com", setGitHub);
+    expect(e.github).tobe(setGitHub);
+})
+
+describe('should get github and return it.', () => {
+    const setGitHub = "DavidLe";
+    const e = new Engineer('David', 123, "davidle@hotmail.com", setGitHub);
+    expect(e.getGithub()).tobe(setGitHub);
+})
+test ('role should return engineer', () => {
+    const roleReturn = "Engineer";
+    const e = new Engineer('David', 123, "davidle@hotmai.com", "DavidLe");
+    expect(e.getRole()).tobe(roleReturn;)
+})
